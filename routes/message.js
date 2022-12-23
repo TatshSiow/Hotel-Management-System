6c173a92bb09a8b333c76dfc3809bba53c56cb94
Tatsh Siow <blussit@gmail.com>
2022-12-23 15:28:07 +0800

    12/23 2nd

--- routes/message.js
+++ routes/message.js
@@ -1,13 +1,10 @@
 var express = require('express');
 var { promisePool: mysql } = require('../lib/mysql');
 var router = express.Router();
-
 router.get('/', async function(req, res, next) {
   const { user } = req.signedCookies;
 
-  const [rows,fields] = await mysql.execute('SELECT * FROM message');
-
-  console.log({ rows });
+  const [rows,fields] = await mysql.execute('SELECT `message`.`message`,`message`.`create_at`, `user`.`username` FROM `message` INNER JOIN `user` ON message.user_id = user.id');
 
   return res.render('message/index', { title: 'Express', user, rows });
 });
