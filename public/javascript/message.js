1e106c8fa2360b79d016ac900f218f99850fe88f
Tatsh <blussit@gmail.com>
2023-05-06 15:32:15 +0800

    20230506

--- public/javascript/message.js
+++ public/javascript/message.js
@@ -22,14 +22,22 @@ const reloadMessage = async () => {
             </div>
         </div>`;
     }
-
+//將 messageList 的內容寫入 HTML中 id 屬性為 message-list 的元素中
     document.querySelector('#message-list').innerHTML = messageList;
 };
 
+//如果我在頁面點了submit按鈕，我的資料會回傳到資料庫
 const submitMessage = async () => {
   const message = document.getElementById('message').value;
   document.getElementById('message').value = '';
 
+/* 向 /message/submit 發送 POST 請求
+將 message 轉換成 string
+fetch 用來發送網路請求的函式，
+用 await 關鍵字來保證等待，執行完後再繼續執行下一步動作
+當伺服器回應時，使用 res.json() 方法將回應的內容解析為 JSON 格式
+並將其儲存在 response 變數中，最後回傳 response 變數的值。
+設定 content-type 為 application/json，表示請求的內容是 JSON 格式。*/
   const response = await fetch('/message/submit', {
     method: 'POST',
     body: JSON.stringify({ message }),
@@ -40,6 +48,7 @@ const submitMessage = async () => {
     return await res.json();
   });
 
+//如果回應值有東西，會刷新message，若無將會提示
   if (response.status) {
     reloadMessage();
   } else {
@@ -47,6 +56,8 @@ const submitMessage = async () => {
   }
 };
 
+//添加submit-message的指令，eventListener是待命操作（click是用戶操作，submitmessage是系統動作）
 document.getElementById('submit-message').addEventListener('click', submitMessage);
 
+//刷新message
 reloadMessage();
